dc536ad3bff5ce7b372c489916121b33ac530653
noorong <100012522+noorong@users.noreply.github.com>
2023-02-22 19:19:40 +0900

    feat: InsertForm UI개발

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import InsertForm from "./components/InsertForm";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <InsertForm />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/InsertForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const InsertForm = () => {
+  return (
+    <form>
+      <input />
+      <button type="submit">등록</button>
+    </form>
+  );
+};
+
+export default InsertForm;
